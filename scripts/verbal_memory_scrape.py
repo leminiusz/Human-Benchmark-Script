fdd8a61194a01b52e13003524a1f0b070320108b
leminiusz <353535@uwr.edu.pl>
2025-07-10 04:35:59 +0200

    i made so everytime web with site opens in full HD(1920,1080)

--- scripts/verbal_memory_scrape.py
+++ scripts/verbal_memory_scrape.py
@@ -9,6 +9,12 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome()
+driver.set_window_size(1920, 1080)  # Set window size to full HD
+#You can also set the window size to your monitor size
+#For example, if you have a 4K monitor, you can set it to
+#But for now you are gonna have to manually set the button positions
+#In the future I want to use pyautogui for the same task by that I mean pyautogui and some kind of image to string
+# recognition to click the buttons based on the images of the buttons
 driver.get("https://humanbenchmark.com/tests/verbal-memory")
 
 seen_words = []
@@ -19,6 +25,9 @@ def click(x, y):
     time.sleep(0.1)
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, 0, 0)
 
+
+click(950, 600)  # start button
+time.sleep(2)  # wait for the game to load
 
 while not keyboard.is_pressed('q'):
     try:
